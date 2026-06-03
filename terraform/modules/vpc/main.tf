41b67f515857a8d52b3b90792fb867d3bdc995e7
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-03 14:15:10 +0100

    public subnets created + output

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -6,3 +6,25 @@ resource "aws_vpc" "vpc" {
     Name = "vpc"
   }
 }
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_block_public1
+  availability_zone = var.az_1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_block_public2
+  availability_zone = var.az_2
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
---
+++ terraform/modules/vpc/output.tf
@@ -0,0 +1,12 @@
+output "aws_vpc" {
+  value = aws_vpc.vpc.id
+}
+
+
+output "public_subnet_1_id" {
+  value = aws_subnet.public_1.id
+}
+
+output "public_subnet_2_id" {
+  value = aws_subnet.public_2.id
+}
