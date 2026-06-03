1610d380937c5fb51cd1d7c2e521066b3edac120
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-03 14:19:21 +0100

    variable cidr block + az included

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,3 +1,18 @@
 variable "vpc_cidr_block" {
   default = "10.0.0.0/22"
 }
+ variable "cidr_block_public1" {
+  default = "10.0.0.0/24"
+}
+
+variable "cidr_block_public2" {
+  default = "10.0.1.0/24"
+}
+
+variable "az_1" {
+  default = "eu-north-1a"
+}
+
+variable "az_2" {
+  default = "eu-north-1b"
+}
