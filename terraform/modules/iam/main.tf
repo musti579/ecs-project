c19f7a0936fb8c419246440efb859c71d6b17167
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-04 19:37:36 +0100

    iam role policy

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -8,3 +8,13 @@ data "aws_iam_policy_document" "ecs_assume_role" {
     }
   }
 }
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = var.ecs_execution_role_name
+  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = var.ecs_task_execution_policy_arn
+}
--- terraform/modules/iam/variable.tf
+++ terraform/modules/iam/variable.tf
@@ -1,3 +1,7 @@
 variable "ecs_execution_role_name" {
   default = "ecsTaskExecutionRole"
 }
+variable "ecs_task_execution_policy_arn" {
+  default = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
