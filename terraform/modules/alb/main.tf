42e7391c453fcd86b35f1eac3a6ebe0ac1718403
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-04 18:26:17 +0100

    alb http listener added

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -62,3 +62,14 @@ resource "aws_lb_target_group" "ecs" {
     Name = "threatmod-tg"
   }
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs.arn
+  }
+}
