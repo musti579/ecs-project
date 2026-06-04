1d89265bc5b2ed7a11fd5b1885567de4e7ae8552
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-04 20:24:55 +0100

    acm cert validation

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -10,4 +10,20 @@ resource "aws_acm_certificate" "cert" {
   lifecycle {
     create_before_destroy = true
   }
+  
+}
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.value]
+  ttl     = 60
 }
