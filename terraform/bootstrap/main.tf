2cd179a37002f4a60b67a1ba9edf7feefc7e941d
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-11 17:23:50 +0100

    Configure GitHub Actions OIDC for AWS authentication

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -9,3 +9,82 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
     status = "Enabled"
   }
 }
+
+data "aws_ecr_repository" "ecs_project" {
+  name = "ecs-project"
+}
+
+// Data Source for ecr //
+
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+
+resource "aws_iam_role" "github_actions" {
+  name = "ecs-project-github-actions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+
+      Action = "sts:AssumeRoleWithWebIdentity"
+
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:musti579/ecs-project:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "github_actions_ecr_push" {
+  name = "ecs-project-github-actions-ecr-push"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["ecr:GetAuthorizationToken"]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage",
+          "ecr:BatchGetImage",
+          "ecr:DescribeRepositories",
+          "ecr:DescribeImages"
+        ]
+        Resource = data.aws_ecr_repository.ecs_project.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_ecr_push" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions_ecr_push.arn
+}
